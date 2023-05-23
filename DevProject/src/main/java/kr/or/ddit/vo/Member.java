c1d0d776aa77dec0d6e1ed7444f1b452015f3667
Seong il Lee <118361080+Sun6004@users.noreply.github.com>
2023-05-23 17:17:57 +0900

    Add files via upload

---
+++ DevProject/src/main/java/kr/or/ddit/vo/Member.java
@@ -0,0 +1,18 @@
+package kr.or.ddit.vo;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Data
+public class Member {
+	private String userName = "hong";
+	private String password = "123";
+	private String userId = "h001";
+	private int coin = 100;
+	@DateTimeFormat(pattern = "yyyyMMdd")
+	private Date dateOfBirth;
+	
+}
