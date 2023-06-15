071ef5a4168d603b543d45057e367461313a8d1f
Seong il Lee <118361080+Sun6004@users.noreply.github.com>
2023-06-15 13:57:51 +0900

    security처리 연습에 DB연결 추가

--- SecurityTest/src/main/java/kr/or/ddit/controller/SecurityController.java
+++ SecurityTest/src/main/java/kr/or/ddit/controller/SecurityController.java
@@ -220,5 +220,130 @@ public class SecurityController {
  *		> 사용자가 정의한 로그인 페이지에서 회원 권한에 해당하는 계정으로 로그인 시, 성공했다면 성공 처리자인 CustomLoginSuccess 클래스로 넘어가
  *			넘겨받은 파라미터들 중 authentication안에 pricipal로 User 정보를 받아서 username과 password를 출력한다.
  *			( 출력 정보는 로그인 성공 시 인증된 회원정보)
+ *
+ *	9. 로그아웃 처리
+ *	로그아웃을 위한 URI를 지정하고, 로그아웃 처리 후에 별도의 작업을 하기 위해서 사용자가 직접 구현한 처리자를 등록할 수 있다.
+ *
+ *		환경설정
+ *		- 스프링 시큐리티 설정
+ *		> security-context.xml설정
+ *		> <security:logoit logout-url="/logout" invalidate-session="true"/>
+ *
+ *		** logout 결로는 스프링에서 제공하는 /logout 경로로 설정한다
+ *		logout 처리 페이지에서도 action경로는 /logout으로 설정한다.
+ *
+ *	10. JDBC를 이용한 인증/인가 처리
+ *	- 지정한 형식으로 테이블을 생성하면 JDBC를 이용해서 인증/인가를 처리할 수 있다.
+ *	생성할 테이블은 사용자를 관리하는 테이블(users)과 권한을 관리하는 테이블 2개로 구성되어 있다.
+ *
+ *		데이터베이스 테이블 준비
+ *		- users, authorities 테이블 준비
+ *
+ *		환경설정
+ *		- 의존 라이브러리 설정
+ *		> 데이터베이스 관련 라이브러리를 추가한다.
+ *		> 기존 데이터베이스 연결을 위한 라이브러리가 추가되어 있다. (SecurityTest 프로젝트 안에는 없기 때문에 새롭게 등록)
+ *
+ *		스프링 설정
+ *		- root-context.xml설정
+ *		> 데이터 소스 설정
+ *
+ *		스프링 시큐리티 설정
+ *		- security-context.xml설정
+ *		> customPasswordEncoder 빈 등록
+ *		>  <security:authentication-managet> 태그 내에 설정
+ *
+ *		비밀번호 암호화 처리기 클래스 정의
+ *		- 비밀번호 암호화 처리기
+ *		> 스프링 시큐리티 5 버전 부터는 기본적으로 PasswordEncoder를 지정해야 하는데,
+ *		 제대로 하려면 생성한 데이터는 암호화를 처리하지 않고 로그인 하면 당연히 로그인 에러가 발생한다.(암호화된 비밀번호가 아니라서)
+ *		 그래서 암호화를 하지 않는 PasswordEncoder를 직접 구현하여 지정하면 로그인 시 암호화를 고려하지 않으므로 로그인이 정상적으로 이루어짐
+ *		- security/CustomNoOpPasswordEncoder 클래스 구현
+ *
+ *	11. 사용자 테이블을 이용한 인증/인가 처리
+ *	스프링 시큐리티가 기본적으로 이용하는 테이블 구조를 그대로 생성해서 사용해도 되지만,
+ *	기존에 구축된 회원 테이블이 있다면 약간의 작업으로 기존 테이블을 활용할 수 있다.
+ *
+ *		db테이블 준비
+ *		- member, member_auth 테이블 준비
+ *
+ *		환경설정
+ *		- 스프링 시큐리티 설정
+ *		- security-context.xml설정
+ *		> bcryptPasswordEncoder 빈 등록 진행
+ *		> <security:jdbc-user-service> 태그 설정
+ *		> <security:password-encoder> 태그 설정
+ *
+ *		쿼리 정의
+ *		- 인증할 때 사용할 쿼리
+ *		> select user_id, user_pw, enabled from member where user_id=?
+ *		- 권한을 확인할 때 사용할 쿼리
+ *		> select m.user_id, ma.auth from member_auth ma, member m where ma.user_no = m.user_no and m.user_id =?
+ *
+ *		** BCryptPasswordEncoder 클래스를 이용하여 직접 encode된 비밀번호를 찾아 데이터베이스에 세팅한다.
+ *		> 12번에 해당하는 내용에서 자세한 내용으로 확인
+ *	
+ *			BCryptPasswordEncoder 클래스를 활용한 단방향 비밀번호 암호화
+ *			- encode() 메서드를 통해서 SHA-2 방식의 8바이트 Hash암호를 매번 랜덤하게 생성한다.
+ *			- 똑같은 비밀번호를 입력하더라도 암호화되는 문자열은 매번 다른 문자열을 반환한다.
+ *
+ *		> 비밀번호를 입력하면 암호화된 비밀번호로 인코딩되는데, 암호화된 비밀번호와 디비 테이블에 있는 암호화된 비밀번호가 일치한지를 파안한 후,
+ *		  일치하면 로그인 성공으로 다음 스탭을 진행한다.
+ *			- BCryptPasswordEncoderm 클래스의 encode() 메서드를 통해 만들어지는 암호화된 해쉬 다이제스트들은 입력한 비밀번호 문자에 해당하는
+ *			   수십억개의 다이제스트들 중에서 일치하는 다이제스트가 존재할 경우 비밀번호의 일치를 보고 인증을 성공시켜준다.
+ *
+ *	12. UserDetailsService 재정의
+ *		스프링 시큐리티의 UserDetailService를 구현하여 사용자 상세 정보를 얻어오는 메서드를 재정의한다.
+ *
+ *		데이터베이스 테이블
+ *		- board 테이블 사용
+ *
+ *		환경 설정
+ *		- 의존 라이브러리 설정(pom.xml)
+ *		> 데이터베이스 관련 라이브러리
+ *
+ *		스프링 시큐리티 설정
+ *		- security-context.xml 설정
+ *		> customUserDetailsService 빈 등록
+ *		> <security:authentication-provider> 태그 설정
+ *
+ *		클래스 재정의
+ *		- UserDetailsService 재정의
+ *		> common.security.CustomUserDetailsService
+ *		> 기존 memberMapper에서 사용중인 read를 기반으로 한 readById를 새롭게 재정의하여 사용하겠음
+ *		> CustomUserDetailsService 클래스 내 loadUserByUsername 메서드에서 인코딩된 비밀번호를 확인해보고 암호화된 비밀번호가 어떻게
+ *			일치하여 인증이 되는지를 같이 알아보도록 한다. 
+ *
+ *	13. 스프링 시큐리티의 표현식
+ *	- 스프링 시큐리티 표현식을 이용하면 인증 및 권한 정보에 따라 화면을 동적으로 구성할 수 있고 로그인 한 사용자 정보를 보여줄 수도 있다.
+ *
+ *		공동 표현식
+ *		- hasRole([role])
+ *			> 해당 롤이 있으면 true
+ *		- hasAnyRole([role1, role2])
+ *			> 여러 롤 중 하나라도 해당하는 롤이 있으면 true
+ *		- principal
+ *			> 안증된 사용자의 사용자 정보(UserDetails 인터페이스를 구현한 클래스의 객체)를 의미
+ *		- authentiction
+ *			> 인증된 사용자의 인증 정보(Authentication 인터페이스를 구현한 클래스의 객체)를 의미
+ *		- permitAll
+ *			> 모든 사용자에게 허용
+ *		- denyAll
+ *			> 모든 사용자에게 거부
+ *		- isAnonymous()
+ *			> 익명의 사용자의 경우(로그인하지 않은 경우도 포함)
+ *		- isAuthenticated()
+ *			> 인증된 사용자면 true
+ *		- ifFullyAuthenticated()
+ *			> Remember-me로 인증된 것이 아닌 일반적인 방법으로 인증된 사용자인 경우 true
+ *
+ *		표현식 사용
+ *		- 표현식을 이용하여 동적 화면 구성
+ *		> home.jsp를 수정한다.
+ *			- 표현식을 이용한 내용을 추가합니다.
+ *		
+ *		- 로그인 한 사용자 정보 보여주기
+ *			> views/board/register.jsp 수정
+ *			> views/notice/register.jsp 수정
  */
 }
