4dbd078c81fa4e4ddf5a2e002c4103dffffadcb7
Seong il Lee <118361080+Sun6004@users.noreply.github.com>
2023-06-16 19:28:38 +0900

    @PreAuthorize 어노테이션으로 접근 제한자 추가

--- SecurityTest/src/main/java/kr/or/ddit/controller/BoardController.java
+++ SecurityTest/src/main/java/kr/or/ddit/controller/BoardController.java
@@ -1,8 +1,16 @@
 package kr.or.ddit.controller;
 
 
+import java.security.Principal;
+import java.util.Iterator;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,9 +29,23 @@ public class BoardController {
 	}
 	
 	// 일반 게시판 등록 화면
+	@PreAuthorize("hasRole('ROLE_MEMBER')")
 	@GetMapping("/register")
-	public String registerForm() {
+	public String registerForm(Principal principal) {
 		log.info("registerForm() 실행...!");
+		// 방법1)
+		log.info("사용자명: "+principal.getName());
+		
+		// 방법2)
+		User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		log.info("username: " +user.getUsername());
+		log.info("password: " +user.getPassword());
+		
+		// 역할명 얻어오기
+		Iterator<GrantedAuthority> ite = user.getAuthorities().iterator();
+		while (ite.hasNext()) {
+			log.info("auth: " + ite.next().getAuthority());	
+		}
 		return "board/register";
 	}
 }
